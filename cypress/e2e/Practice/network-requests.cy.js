9058bd7be32ee3ebf9e82eda332e3643c8385685
VJ <vincent-joossen@hotmail.com>
2024-01-20 23:31:22 +0100

    (Tests) Solve network requests

--- cypress/e2e/Practice/network-requests.cy.js
+++ cypress/e2e/Practice/network-requests.cy.js
@@ -21,11 +21,17 @@ describe("Network Requests", () => {
     // returns a status code of 200
     // Hint: You will need to use cy.request()
     // https://docs.cypress.io/api/commands/request
+    cy.request("GET", "/api/posts").then((response) => {
+      expect(response.status).to.eq(200);
+    });
   });
 
   it("/api/posts returns the correct number of posts", () => {
     // Write an assertion that the route '/api/posts'
     // returns the correct number of posts.
+    cy.request("GET", "/api/posts").then((response) => {
+      expect(response.body.length).to.eq(2);
+    });
   });
 
   it("the posts.json fixture returns the correct number of posts", () => {
@@ -34,6 +40,9 @@ describe("Network Requests", () => {
     // There are 25 total posts in the fixture
     // Hint: You will need to use cy.fixture()
     // https://docs.cypress.io/api/commands/fixture
+    cy.fixture("posts.json").then((fixture) => {
+      expect(fixture.length).to.eq(25);
+    });
   });
 
   it("intercepts /api/posts and returns the correct number of posts", () => {
@@ -41,5 +50,6 @@ describe("Network Requests", () => {
     // and assert that the response contains the correct number of posts
     // Hint: you will need to cy.wait() to wait upon the @posts alias.
     // https://docs.cypress.io/api/commands/wait
+    cy.wait("@posts").its("response.body").should("have.length", 2);
   });
 });
